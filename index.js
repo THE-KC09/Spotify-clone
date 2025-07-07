a92e1498b43b35fe658b73dfafd1c717f155a828
THE KC09 <thekc2083@gmail.com>
2025-07-07 13:17:17 +0530

    progress5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Now it's time of javaScript");
+
+// async function getSongs(){
+
+//     const query = document.querySelector(".songSearch");
+//     let url = await fetch(`https://saavn.dev/api/songs?query=${encodeURIComponent(query)}`);
+//     const respone = await url.json();
+
+//     if(respone.data.results.length === 0){
+//         document.querySelector("input").placeholder = "Song Not found!!";
+//         return;
+
+//     }
+
+//     const song = respone.data.results[0];
+//     const audioUrl = song.downloadUrl[4].link;
+
+//     document.querySelector(".songInfo").innerHTML = `<p><strong>${song.name}"</strong> by ${song.primaryArtists}</p>
+//     <audio controls autoplay src = "${audioUrl}</audio>"`;
+// }
